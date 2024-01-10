ae84d2a6ad4f560d01f53c116244a53715c47b45
huizhang43 <huizhang43@iflytek.com>
2024-01-10 11:12:07 +0800

    修改左边栏目录

--- huasen-frontend/portal/src/config/nav.config.js
+++ huasen-frontend/portal/src/config/nav.config.js
@@ -7,33 +7,23 @@
  */
 const navs = [
   {
-    name: '极简主页',
+    name: '主页',
     path: '/home',
     iconClass: 'iconfont icon-iconset0278',
   },
   {
-    name: '文章笔记',
-    path: '/article',
-    iconClass: 'iconfont icon-iconset0156',
-  },
-  {
     name: '博客平台',
     path: '/blog',
-    iconClass: 'iconfont icon-iconset0455',
+    iconClass: 'iconfont icon-iconset0156',
   },
   {
     name: '聊天工具',
     path: '/chat',
-    iconClass: 'iconfont icon-iconset0352',
+    iconClass: 'iconfont icon-iconset0455',
   },
   {
     name: '待办清单',
     path: '/todo',
-    iconClass: 'iconfont icon-iconset0133',
-  },
-  {
-    name: '有道笔记',
-    path: '/youdao',
     iconClass: 'iconfont icon-iconset0448',
   }
 ];
