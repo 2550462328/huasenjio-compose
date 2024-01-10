68049b6060b91e48e543146e1ac560f48218df9e
huizhang43 <huizhang43@iflytek.com>
2024-01-10 10:56:26 +0800

    添加有道笔记和todolist

--- huasen-frontend/portal/src/config/nav.config.js
+++ huasen-frontend/portal/src/config/nav.config.js
@@ -2,7 +2,7 @@
  * @Autor: huasenjio
  * @Date: 2022-02-11 00:45:10
  * @LastEditors: huizhang43 huizhang43@iflytek.com
- * @LastEditTime: 2024-01-02 16:58:16
+ * @LastEditTime: 2024-01-10 10:39:00
  * @Description: 侧边导航栏配置
  */
 const navs = [
@@ -25,6 +25,16 @@ const navs = [
     name: '聊天工具',
     path: '/chat',
     iconClass: 'iconfont icon-iconset0352',
+  },
+  {
+    name: '待办清单',
+    path: '/todo',
+    iconClass: 'iconfont icon-iconset0133',
+  },
+  {
+    name: '有道笔记',
+    path: '/youdao',
+    iconClass: 'iconfont icon-iconset0448',
   }
 ];
 export default navs;
---
+++ huasen-frontend/portal/src/views/third/Todo.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: huizhang43 huizhang43@iflytek.com
+ * @Date: 2024-01-10 10:41:19
+ * @LastEditors: huizhang43 huizhang43@iflytek.com
+ * @LastEditTime: 2024-01-10 10:41:56
+ * @FilePath: \huasen-frontend\portal\src\views\third\Todo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div class="container">
+        <object :data="url" type="text/html" width="100%" height="100%"></object>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {url: "https://app.todoist.com/app/today",}
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        position: relative;
+        height: 100% !important;
+    }
+</style>
