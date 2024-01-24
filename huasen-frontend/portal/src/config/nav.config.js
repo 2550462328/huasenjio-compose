ec4f3bb7f22ac4445e33bf3aca6cc939c3c9e22a
huizhang43 <huizhang43@iflytek.com>
2024-01-24 15:36:40 +0800

    删除 博客、聊天侧边栏

--- huasen-frontend/portal/src/config/nav.config.js
+++ huasen-frontend/portal/src/config/nav.config.js
@@ -2,7 +2,7 @@
  * @Autor: huasenjio
  * @Date: 2022-02-11 00:45:10
  * @LastEditors: huizhang43 huizhang43@iflytek.com
- * @LastEditTime: 2024-01-10 10:39:00
+ * @LastEditTime: 2024-01-16 10:03:07
  * @Description: 侧边导航栏配置
  */
 const navs = [
@@ -12,14 +12,9 @@ const navs = [
     iconClass: 'iconfont icon-iconset0278',
   },
   {
-    name: '博客平台',
-    path: '/blog',
+    name: '文章',
+    path: '/article',
     iconClass: 'iconfont icon-iconset0156',
-  },
-  {
-    name: '聊天工具',
-    path: '/chat',
-    iconClass: 'iconfont icon-iconset0455',
   },
   {
     name: '待办清单',
